ee702ce1364576a9df5f8b1f92ae41eaab0ac0fc
teebaron <sgraba@web.de>
2020-11-30 22:58:19 +0100

    fix

--- script.js
+++ script.js
@@ -46,8 +46,8 @@ var door_nr_array = [
     "🦌☃️🦌🦌🦌"];
 
 var counter = document.getElementById("counter");
-// const curDay = 24-days_to_xmas();
-const curDay = 5;
+const curDay = 24-days_to_xmas();
+// const curDay = 5;
 console.log("Today: " + curDay);
 counter.innerHTML = days_to_xmas();
 
