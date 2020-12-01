2b30951afbbd21a74d3c4bcb6612686c6dee4b2a
teebaron <sgraba@web.de>
2020-12-01 12:32:40 +0100

    fix days

--- script.js
+++ script.js
@@ -118,9 +118,7 @@ function days_to_xmas() {
     const ONE_DAY = 1000 * 60 * 60 * 24;
 
     // Calculate the difference in milliseconds
-    const differenceMs = Math.abs(xmas - now);
-
-    // Convert back to days and return
-    return Math.round(differenceMs / ONE_DAY);
+    const diff = Math.ceil((xmas.getTime()-now.getTime())/(ONE_DAY));
+    return diff;
 
 }
